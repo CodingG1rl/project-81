f8108e56a07c6382407609efd50d7f91e722cbed
CodingG1rl <86166581+CodingG1rl@users.noreply.github.com>
2021-09-27 16:53:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+canvas=document.getElementById("my_canvas");
+
+ctx= canvas.getcontext(2d);
+ctx.beginpath();
+ctx.strokestyle= red;
+ctx.linewidth= 2;
+ctx.rect(150,143,430,200,2);
+ctx.stroke();
+
+ 
+ctx.beginpath();
+ctx.strokestyle= blue;
+ctx.linewidth= 2;
+ctx.arc(275,210,40,0,2*MATH.PI);
+
+ctx.beginpath();
+ctx.strokestyle= black;
+ctx.linewidth= 2;
+ctx.arc(375,210,40,0,2*MATH.PI);
+
+ctx.beginpath();
+ctx.strokestyle= red;
+ctx.linewidth= 2;
+ctx.arc(475,210,40,0,2*MATH.PI
+    
+    ctx.beginpath();
+ctx.strokestyle= yellow;
+ctx.linewidth= 2;
+ctx.arc(330,250,40,0,2*MATH.PI);
+
+ctx.beginpath();
+ctx.strokestyle= green;
+ctx.linewidth= 2;
+ctx.arc(430,250,40,0,2*MATH.PI);
